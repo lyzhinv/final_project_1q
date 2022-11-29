fd0513b9e87b3e345b1ae5cdf8e92c87aeeff230
Vadim <lyzhinv@gmail.com>
2022-11-29 15:52:54 +0300

    Add Exercise 4

---
+++ 4_Programm/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] basearray = { "hello", "2", "world", ":-)" };
+int size = 3;
+
+Console.Write($"{String.Join(", ", basearray)} -> ");
+GetResult(basearray, size);
+
+void GetResult(string[] array, int size) // Метод, который находит все элементы строкового массива, больше или равно заданной длины 
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+         if (array[i].Length <= size) Console.Write($"{array[i]}, ");
+    }
+    
+}
